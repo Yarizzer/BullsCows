557633f13c641f7eb7b5fef474d61748b56e6005
Yaroslav Abaturov <yarizzer@gmail.com>
2023-04-17 18:33:48 +0300

    Initial Commit

---
+++ BullsCows/BullsCowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsCowsApp.swift
+//  BullsCows
+//
+//  Created by Yaroslav Abaturov on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BullsCowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
