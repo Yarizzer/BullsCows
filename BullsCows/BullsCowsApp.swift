258caf4e898d55740b81d5d20b1e756eca217f09
Yaroslav Abaturov <yarizzer@gmail.com>
2023-04-17 20:24:59 +0300

    added list with test data

--- BullsCows/BullsCowsApp.swift
+++ BullsCows/BullsCowsApp.swift
@@ -13,5 +13,6 @@ struct BullsCowsApp: App {
         WindowGroup {
             ContentView()
         }
+        .windowResizability(.contentSize)
     }
 }
--- BullsCows/ContentView.swift
+++ BullsCows/ContentView.swift
@@ -8,19 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    #warning("test section")
+    private let testData = Array(repeating: "1234", count: 20)
+    #warning("test section")
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                TextField("Enter a guess…", text: .constant("1234"))
+                Button("Go", action: submitGuess)
+            }
+            .padding()
+            .frame(width: Constants.frame.width)
+//            .frame(height: Constants.frame.height)
+            List(testData, id: \.self) { guess in
+                HStack {
+                    Text(guess)
+                    Spacer()
+                    Text("4c 9b")
+                }
+            }
         }
-        .padding()
+    }
+    
+    private func submitGuess() {
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension ContentView {
+    private struct Constants {
+        static let frame: (width: CGFloat, height: CGFloat) = (width: 250.0, height: 300.0)
     }
 }
